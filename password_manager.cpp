7170c7725bf339494ee5adeef2262be2bac01876
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-04-14 18:34:32 +0530

    Add files via upload

---
+++ password_manager.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Welcome to Password Manager\n"
+        <<"What would you like to do?\n"
+        <<"1.)Generate Random Password.\n"
+        <<"2.)Encript a message.\n"
+        <<"2.)Decript a message.\n";
+    int choice{};
+    cin>>choice;
+    return 0;
+}
