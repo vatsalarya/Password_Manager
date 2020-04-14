2995285048f10b2e6c260bc2ca3761efd66f495f
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-04-14 18:40:37 +0530

    Update password_manager.cpp

--- password_manager.cpp
+++ password_manager.cpp
@@ -4,8 +4,8 @@ int main(){
     cout<<"Welcome to Password Manager\n"
         <<"What would you like to do?\n"
         <<"1.)Generate Random Password.\n"
-        <<"2.)Encript a message.\n"
-        <<"2.)Decript a message.\n";
+        <<"2.)Encrypt a message.\n"
+        <<"2.)Decrypt a message.\n";
     int choice{};
     cin>>choice;
     return 0;
