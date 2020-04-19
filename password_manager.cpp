046075fa03bf6684ceb59c9d03e4a3af0d1e78d0
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-04-19 05:44:16 +0000

    Update password_manager.cpp

--- password_manager.cpp
+++ password_manager.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+
+string RandomPassword(int n);
+
 int main(){
     cout<<"Welcome to Password Manager\n"
         <<"What would you like to do?\n"
@@ -8,5 +11,24 @@ int main(){
         <<"2.)Decrypt a message.\n";
     int choice{};
     cin>>choice;
+    if(choice == 1){
+        int n{};
+        cout<<"Enter length of password: ";
+        cin>>n;
+        string pass;
+        pass = RandomPassword(n);
+        cout<<"The Random Password generated is: "
+        for(auto p : pass)
+            cout<<p;
+        cout<<endl;
+    }
     return 0;
+}
+
+string RandomPassword(int n){
+    srand(time(nullptr));
+    string s;
+    while(n--)
+        s.push_back(rand()%127+48);
+    return s;
 }
