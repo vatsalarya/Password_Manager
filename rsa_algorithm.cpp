8d82a75e699ef2fb7406400c075bc39534cb7317
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-04-25 10:10:04 +0000

    Update rsa_algorithm.cpp

--- rsa_algorithm.cpp
+++ rsa_algorithm.cpp
@@ -1,58 +1,62 @@
-#include<iostream>
+#include<bits/stdc++.h>
 #include<stdio.h>
 using namespace std;
+
+double p,q,n,e,phi,d;
+int k;
 
 int gcd(int a, int b) {
 	if(b == 0)
 		return a;
-	return gcd_fast(b , a%b);
+	return gcd(b , a%b);
 
 }
-int main() { 
-    // Two random prime numbers 
-    double p = 7; 
-    double q = 11; 
-  
-    // First part of public key: 
-    double n = p*q; 
-  
-    // Finding other part of public key. 
-    // e stands for encrypt 
-    double e = 2; 
-    double phi = (p-1)*(q-1); 
-    while (e < phi) 
-    { 
-        // e must be co-prime to phi and 
-        // smaller than phi. 
-        if (gcd(e, phi)==1) 
-            break; 
-        else
-            e++; 
-    } 
-  
-    // Private key (d stands for decrypt) 
-    // choosing d such that it satisfies 
-    // d*e = 1 + k * totient 
-    int k = 2;  // A constant value 
-    double d = (1 + (k*phi))/e; 
-  
-    // Message to be encrypted 
-    string message;
-   string encrypted_message;
-	
-string decrypted_message;
-  for(auto msg : message){
+
+string Encrypt_message(string message){
+	string encrypted_message;
+  	for(char p : message){
     // Encryption c = (msg ^ e) % n 
-    char c = pow(msg, e); 
+    char c = pow(p, e); 
     c = fmod(c, n); 
-    printf("Encrypted data = %s", c); 
-  }for(auto msg 
-	
+    encrypted_message += c;; 
+	}
+    return encrypted_message;
+}
+string Decrypt_message(string message){
+	string decrypted_message;
+	for(auto c : message){
     // Decryption m = (c ^ d) % n 
     double m = pow(c, d); 
     m = fmod(m, n); 
-    printf("\nOriginal Message Sent = %lf", m); 
+    decrypted_message += m;
+	}
+	return decrypted_message;
+
+}
+void create_pair(){
+	p = 7; 
+    q = 11; 
   
+    n = p*q; 
+  
+    e = 2; 
+    phi = (p-1)*(q-1); 
+    while (e < phi) { 
+        if (gcd(e, phi)==1) break; 
+        else e++; 
+    } 
+  
+    // choosing d such that it satisfies d*e = 1 + k * totient 
+    k = 2;  // A constant value 
+    d = (1 + (k*phi))/e; 
+  
+}
+int main() { 
+	create_pair();
+    string message;
+	cin>>message;
+	cout<<Encrypt_message(message)<<endl<<Decrypt_message(Encrypt_message(message));
+	
+	
     return 0; 
-} 
-// This code is contributed by Akash Sharan. 
+}
